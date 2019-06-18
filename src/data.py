e83d4603f6550bad26faaae036dc943fd00c041d
Akhil Pandey <akhilpandey95@icloud.com>
2019-06-17 20:35:20 -0500

    Issue #11: Segregated the model structure (#12)
    - all the model architectures will reside in src/models.py
    - all the data processing methods will reside in src/data.py
    - all the evaluation methods will reside in src/evaluation.py
    - src/sch_imp_nn_exp3.py would use the above helper methods for training
    the model and evaluating the same
    
    Signed-off-by: akhilpandey95 <akhilpandey95@icloud.com>

---
+++ src/data.py
@@ -0,0 +1,100 @@
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/akhilpandey95/scholarlyimpact/blob/master/LICENSE.
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+
+# function for processing the dataset
+def data_processing(file_path):
+    """
+    Process the dataset and prepare it for using it against the neural network model
+
+    Parameters
+    ----------
+    arg1 | file_path: str
+        The file path indicating the location of the dataset
+
+    Returns
+    -------
+    Dataframe
+        pandas.DataFrame
+
+    """
+    try:
+        # read the dataset
+        data = pd.read_csv(file_path, low_memory=False)
+
+        # create the label encoder
+        encoder = LabelEncoder()
+
+        # transform all the columns
+        data.Type = encoder.fit_transform(data.Type)
+
+        # transform the column profession
+        data.Profession = encoder.fit_transform(data.Profession)
+
+        # transform the column academic status
+        data.AcademicStatus = encoder.fit_transform(data.AcademicStatus)
+
+        # transform the column platform with max mentions
+        data.PlatformWithMaxMentions = encoder.fit_transform(data.PlatformWithMaxMentions)
+
+        # create a target variable for the first experiment
+        data = data.assign(target_exp_1 =
+                           list(map(lambda x: 1 if x > 0 else 0, tqdm(data['citations']))))
+
+        # create a target variable for the first experiment
+        data = data.assign(target_exp_2 =
+                           list(map(lambda x: 1 if x > 9 else 0, tqdm(data['citations']))))
+
+        # create a target variable for the first experiment
+        data = data.assign(target_exp_3 =
+                           list(map(lambda x: np.log(1 + x), tqdm(data['citations']))))
+
+        # drop the columns unecessary
+        data = data.drop(columns=['Type', 'citations', 'citations(Log_Transformed)'])
+
+        # return the dataframe
+        return data
+    except:
+        return pd.DataFrame()
+
+# function for preparing the X & Y for the dataset
+def prepare_X_Y(data_frame):
+    """
+    Process the dataframe and return the X and Y for the experiment
+
+    Parameters
+    ----------
+    arg1 | data_frame: pandas.DataFrame
+        A loaded dataframe for preparing X and Y
+
+    Returns
+    -------
+    Tuple
+        numpy.ndarray, numpy.ndarray
+
+    """
+    try:
+        # the following data columns will be considered as features
+        data_columns = ['mendeley', 'citeulike', 'News', 'Blogs',
+                        'Reddit', 'Twitter', 'Facebook',
+                        'GooglePlus', 'PeerReviews','Wikipedia',
+                        'TotalPlatforms', 'SincePublication','PlatformWithMaxMentions',
+                        'Countries', 'MaxFollowers', 'Retweets','Profession',
+                        'AcademicStatus', 'PostLength', 'HashTags', 'Mentions',
+                        'AuthorCount']
+
+        # set the X column
+        X = data_frame.as_matrix(columns = data_columns)
+        
+        # set the target variable
+        Y = data_frame.target_exp_3
+
+        # return the tuple
+        return X, Y
+    except:
+        return np.zeros((len(data_frame), 22)), np.zeros((len(data_frame), 22))
---
+++ src/models.py
@@ -0,0 +1,113 @@
+# This Source Code Form is subject to the terms of the MIT
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/akhilpandey95/scholarlyimpact/blob/master/LICENSE.
+
+import tensorflow.keras as keras
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+# feedforward network for predicting log(1 + citations)
+class PredictLogCitation(Model):
+    """
+    Class object for predicting Log(1 + citations) for a given
+    scholarly paper
+
+    Parameters
+    ----------
+    No arguments
+
+    Returns
+    -------
+    Neural Network Model
+        keras.model.Model
+
+    """
+    # function for preparing the X & Y for the dataset
+    def __init__(self):
+        """
+        Build the Vanilla style neural network model and compile it
+
+        Parameters
+        ----------
+        No arguments
+
+        Returns
+        -------
+        Nothing
+            None
+
+        """
+        # super class the keras model
+        super(PredictLogCitation, self).__init__()
+
+        # create the model
+        self.model = Sequential()
+
+        # add the first hidden layer with 32 neurons, relu activation
+        self.model.add(Dense(32, activation='relu', input_dim=22))
+
+        # add the second hidden layer with 64 neurons, relu activation
+        self.model.add(Dense(64, activation='relu'))
+
+        # add the third hidden layer with 64 neurons, relu activation
+        self.model.add(Dense(64, activation='relu'))
+
+        # add the fourth hidden layer with 64 neurons, relu activation
+        self.model.add(Dense(64, activation='relu'))
+
+        # add the fifth hidden layer with 32 neurons, relu activation
+        self.model.add(Dense(32, activation='relu'))
+
+        # add the single output layer
+        self.model.add(Dense(1))
+
+        # use the rmsprop optimizer
+        self.rms = keras.optimizers.RMSprop(lr=0.001)
+
+        # compile the model
+        self.model.compile(optimizer=self.rms, loss='mean_squared_error', metrics =['mean_absolute_error'])
+
+    # function for training the neural network model
+    def train(self, epochs, batch_size, X_train, X_test, Y_train, Y_test, stopping=True):
+        """
+        Fit the neural network model
+
+        Parameters
+        ----------
+        arg1 | model: keras.model.Model
+            A compiled keras neural network model to train
+        arg2 | X_train: numpy.ndarray
+            The training samples containing all the predictors
+        arg3 | X_test: numpy.ndarray
+            The test samples containing all the predictors
+        arg4 | Y_train: numpy.ndarray
+            The training samples containing values for the target variable
+        arg5 | Y_test: numpy.ndarray
+            The test samples containing values for the target variable
+        arg6 | stopping: boolean
+        A flag asserting if early stopping should or shouldn't be used for training
+
+        Returns
+        -------
+        Neural Network Model
+            keras.model.Model
+
+        """
+        try:
+            if not stopping:
+                # fit the model
+                self.model.fit(X_train, Y_train, epochs=epochs, validation_split=0.2, batch_size=batch_size)
+            else:
+                # prepare for early stopping
+                early_stopping = keras.callbacks.EarlyStopping(monitor='mean_squared_error', min_delta=0,
+                                                         patience=40, verbose=0, mode='auto',
+                                                         baseline=None, restore_best_weights=False)
+                # fit the model
+                self.model.fit(X_train, Y_train, epochs=epochs, validation_split=0.2, batch_size=batch_size, callbacks=[early_stopping])
+
+            # return the model
+            return self.model
+        except:
+            return keras.models.Model()
