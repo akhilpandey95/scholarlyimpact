f2d10285284d61abcdf0bfe9f5861c45ae55f6eb
Akhil Pandey <akhilpandey95@icloud.com>
2019-06-29 23:53:11 -0500

    Issue #4: Added neural network model for exp 1 (#15)
    - added a three layered feed forward network for predicting if a
    scholarly article contains citations or not
    - added a gitignore file
    
    Signed-off-by: akhilpandey95 <akhilpandey95@icloud.com>

--- src/models.py
+++ src/models.py
@@ -111,3 +111,101 @@ class PredictLogCitation(Model):
             return self.model
         except:
             return keras.models.Model()
+
+# feedforward network for predicting if citations exist or not
+class PredictCitationsExist(Model):
+    """
+    Class object for predicting if citations for a given
+    scholarly paper exist or not
+
+    Parameters
+    ----------
+    No arguments
+
+    Returns
+    -------
+    Neural Network Model
+        keras.model.Model
+
+    """
+    # function for preparing the X & Y for the dataset
+    def __init__(self):
+        """
+        Build the Vanilla style neural network model and compile it
+
+        Parameters
+        ----------
+        No arguments
+
+        Returns
+        -------
+        Nothing
+            None
+
+        """
+        # super class the keras model
+        super(PredictCitationsExist, self).__init__()
+
+        # create the model
+        self.model = Sequential()
+
+        # add the first hidden layer with 64 neurons, relu activation
+        self.model.add(Dense(64, activation='sigmoid', input_dim=22))
+
+        # add the second hidden layer with 128 neurons, relu activation
+        self.model.add(Dense(128, activation='selu'))
+
+        # add the third hidden layer with 64 neurons, relu activation
+        self.model.add(Dense(64, activation='sigmoid'))
+        
+        # add the single output layer
+        self.model.add(Dense(1, activation='sigmoid'))
+
+        # use the rmsprop optimizer
+        self.rms = keras.optimizers.RMSprop(lr=0.001)
+
+        # compile the model
+        self.model.compile(optimizer=self.rms, loss='binary_crossentropy', metrics =['accuracy'])
+
+    # function for training the neural network model
+    def train(self, epochs, batch_size, X_train, X_test, Y_train, Y_test, stopping=True):
+        """
+        Fit the neural network model
+
+        Parameters
+        ----------
+        arg1 | model: keras.model.Model
+            A compiled keras neural network model to train
+        arg2 | X_train: numpy.ndarray
+            The training samples containing all the predictors
+        arg3 | X_test: numpy.ndarray
+            The test samples containing all the predictors
+        arg4 | Y_train: numpy.ndarray
+            The training samples containing values for the target variable
+        arg5 | Y_test: numpy.ndarray
+            The test samples containing values for the target variable
+        arg6 | stopping: boolean
+        A flag asserting if early stopping should or shouldn't be used for training
+
+        Returns
+        -------
+        Neural Network Model
+            keras.model.Model
+
+        """
+        try:
+            if not stopping:
+                # fit the model
+                self.model.fit(X_train, Y_train, epochs=epochs, validation_split=0.2, batch_size=batch_size)
+            else:
+                # prepare for early stopping
+                early_stopping = keras.callbacks.EarlyStopping(monitor='binary_cross_entropy', min_delta=0,
+                                                         patience=40, verbose=0, mode='auto',
+                                                         baseline=None, restore_best_weights=False)
+                # fit the model
+                self.model.fit(X_train, Y_train, epochs=epochs, validation_split=0.2, batch_size=batch_size, callbacks=[early_stopping])
+
+            # return the model
+            return self.model
+        except:
+            return keras.models.Model()
---
+++ src/sch_imp_nn_exp1.py
@@ -0,0 +1,51 @@
+# This Source Code Form is subject to the terms of the MIT
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/akhilpandey95/scholarlyimpact/blob/master/LICENSE.
+
+import sys
+from models import PredictCitationsExist
+from evaluation import evaluate, metrics
+from data import data_processing, prepare_X_Y
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    # load the dataset
+    data = data_processing('~/Downloads/sch_impact.csv')
+
+    # prepare the X, Y
+    X, Y = prepare_X_Y(data)
+
+    # build the train and test samples
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+
+    # build the model
+    classifier = PredictCitationsExist()
+
+    # train the model
+    classifier = classifier.train(10, 64, X_train, X_test, Y_train, Y_test, stopping=False)
+
+    # evaluate and print the training stats
+    model_evaluation = evaluate(classifier, 'train', x_train=X_train, y_train=Y_train)
+
+    # print training metrics
+    print('Training Loss:', model_evaluation[0])
+    print('Training Accuracy:', model_evaluation[1])
+
+    # print the test set metrcs [acc, prec, recall, f1]
+    model_evaluation = metrics(classifier, x_test=X_test, y_test=Y_test)
+    
+    # test accuracy
+    print('Test accuracy:', model_evaluation[0])
+
+    # precision
+    print('Precision:', model_evaluation[1])
+
+    # recall
+    print('Recall:', model_evaluation[2])
+    
+    # f1
+    print('F-1:', model_evaluation[3])
+else:
+    print('ERR: unable to run the script')
+    sys.exit(0)
