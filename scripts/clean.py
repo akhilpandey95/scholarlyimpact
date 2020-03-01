a8552b4107c87f7bea01cfffb92af400547feb2a
akhilpandey95 <akhilpandey.akella@gmail.com>
2020-02-29 23:58:41 -0600

    Issue #30: Updated citation extraction script
    - updated gitignore to ignore txt and csv files from scripts directory
    - split the citation extraction script into smaller scripts
    - added a bash script to run the python script in backgorund
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

---
+++ scripts/clean.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the MIT
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/akhilpandey95/scholarlyimpact/blob/master/LICENSE.
+
+import os
+
+os.remove('altmetric_ids.txt')
+os.remove('citations_dim.csv')
